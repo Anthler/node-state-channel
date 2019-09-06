b7b38095bbaf750e872103e01a62d9d159d4dca0
Anthler <bullanthla@gmail.com>
2019-09-06 03:50:49 -0400

    Added transaction verification and validation endpoint

--- index.js
+++ index.js
@@ -1,61 +1,94 @@
 const express = require("express");
 const Web3 = require("web3");
-const ethereumjs = require("ethereumjs-util");
 
-const web3 = new Web3();
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/5a705e38d7704b26a676c46394ab8920"
+  )
+);
+
 const app = express();
+app.use(express.json());
 
+const CONTRACT_ADDR = "0x890575Aee83E2b50869B3917A77a5578b86b0e98";
 const users = [
-  { id: 1, balance: 300, password: 12345, nonces: 0 },
-  { id: 2, balance: 200, password: 67890, nonces: 0 }
+  {
+    id: 1,
+    balance: 300,
+    address: "0x890575Aee83E2b50869B3917A77a5578b86b0e98",
+    password: "Anthler01",
+    nonces: 0
+  },
+  {
+    id: 2,
+    balance: 200,
+    address: "0xB36023D6626841e825b99eF410F2fB84a9B9c970",
+    password: "Anthler01",
+    nonces: 0
+  }
 ];
 
-app.post("/user/transaction/sign/:id", async (req, res) => {
+app.post("/users/transactions/sign/:id", async (req, res) => {
   //extract the user's ID from request parameters
-  const id = req.params.id;
 
-  //Loop through the users array to find the
-  const user = users.find(u => u.id === parseInt(id));
+  try {
+    const id = req.params.id;
 
-  // Check to see if the user id provided exist for a user in our users array
-  if (!user) return res.status(404).send(" Invlid user ID provided");
+    //Loop through the users array to find the
+    const user = users.find(u => u.id === parseInt(id));
 
-  //Get balance of the user signing transaction
-  const balance = user.balance;
+    // Check to see if the user id provided exist for a user in our users array
+    if (!user) return res.status(404).send(" Invlid user ID provided");
 
-  //Get amount sent from the request body
-  const amount = req.body.amount;
+    let { balance, address, password, nonces } = user;
 
-  //check to see if the sender has enough balance to sign a transaction
-  if (balance < amount) return res.status(400).send("Insufficient balance");
+    //Get amount sent from the request body
+    const { amount, recipient } = req.body;
 
-  //Get the nonce to be used for the transaction
-  const nonce = (user.nonces += 1);
+    //check to see if the sender has enough balance to sign a transaction
+    if (balance < amount) return res.status(400).send("Insufficient balance");
 
-  // Hash the transaction message to be signed
-  const hash = ethereumjs.ABI.soliditySHA3(
-    ["address", "uint256", "uint256"],
-    [web3.eth.defaultAccount, amount, nonce]
-  ).toString("hex");
+    //Get the nonce to be used for the transaction
+    nonce = nonces += 1;
 
-  // Sign the transaction
-  const signature = await web3.eth.personal.sign(
-    hash,
-    web3.eth.defaultAccount,
-    user.password
-  );
+    //Hash the transaction message to be signed
+    const hash = await web3.utils.soliditySha3(
+      recipient,
+      amount,
+      nonce,
+      CONTRACT_ADDR
+    );
 
-  //The current/ updated balance after transaction is signed
-  const currentBalance = balance - amount;
+    // Sign the transaction
+    const signature = await web3.eth.personal.sign(hash, address, password);
 
-  //Update the signer balance to reflect current balance
-  user.balance = currentBalance;
+    //The current/ updated balance after transaction is signed
+    balance -= amount;
 
-  //Send the signed signature to the signer to be sent to the recipient
-  res.status(200).send(signature);
+    //Send the signed signature to the signer to be sent to the recipient
+    res.status(200).send(hash, signature, nonce);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.use("/transaction/verify", (req, res) => {});
+app.use("/transactions/sender/verify", async (req, res) => {
+  const { signature, expected_sender, nonce, amount } = req.body;
+
+  const hash = await web3.utils.soliditySha3(
+    recipient,
+    amount,
+    nonce,
+    CONTRACT_ADDR
+  );
+  const signer = await web3.eth.personal.ecRecover(hash, signature);
+
+  if (expected_sender !== signer) {
+    return res.send(404).send("Signature is invalid");
+  }
+
+  res.status(200).send({ valid: true });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
